384932353308fab22626750124709f416ac2ace7
Ammar <ammar.muhammad@siemens.com>
2024-11-10 10:57:21 +0100

    making the repo pip installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz",
+    version="0.1.0",
+    description="A fun math game",
+    author="Ammar",
+    author_email="muhammad.ammar@fau.de",
+    packages=find_packages(),
+    install_requires=[],
+    zip_safe=False,
+)
